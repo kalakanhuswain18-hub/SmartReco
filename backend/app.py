2af253c4832c5a18e8c5b6001a8871f865fa7575
kalakanhuswain18-hub <kalakanhuswain18@gmail.com>
2026-08-11 10:45:15 +0530

    Configure frontend URL for CORS

--- backend/app.py
+++ backend/app.py
@@ -8,25 +8,24 @@ from routes.events import events_bp
 from routes.auth import auth_bp
 from routes.wishlist import wishlist_bp
 from routes.orders import orders_bp
-
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
-CORS(app)
+CORS(app, origins=[os.getenv("FRONTEND_URL")])
 
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
-    app.register_blueprint(products_bp)
-    app.register_blueprint(recommendation_bp)
-    app.register_blueprint(events_bp)
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(wishlist_bp)
-    app.register_blueprint(orders_bp)
 
+app.register_blueprint(products_bp)
+app.register_blueprint(recommendation_bp)
+app.register_blueprint(events_bp)
+app.register_blueprint(auth_bp)
+app.register_blueprint(wishlist_bp)
+app.register_blueprint(orders_bp)
 
 @app.route("/")
 def home():
@@ -35,7 +34,6 @@ def home():
         "status": "Running",
         "message": "Welcome to SmartReco API"
     }
-
 
 if __name__ == "__main__":
     app.run(debug=True)
